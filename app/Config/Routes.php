83420b523f82627c1fe6d7600e338b2d370b07c0
pantiarso <bonder995@gmail.com>
2023-10-24 17:08:36 +0700

    update auth

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'BukuController::index');
-$routes->get('buku', 'BukuController::index');
-$routes->get('detail_buku/(:segment)', 'DetailBukuController::index/$1');
-$routes->get('rating_buku', 'RatingBukuController::index');
+$routes->group('admin', function ($routes) {
+    $routes->get('dashboard', 'BukuController::index');
+    $routes->get('buku', 'BukuController::index');
+    $routes->get('detail_buku/(:segment)', 'DetailBukuController::index/$1');
+    $routes->get('rating_buku', 'RatingBukuController::index');
+});
